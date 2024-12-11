c3dd0d951b2d3d8653762628d1bda6710d74a3b8
vipink28 <vipinkalyan28@gmail.com>
2024-12-11 13:48:35 +0530

    login function added

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -9,6 +9,21 @@ const Login = () => {
             [name]: value
         }))
     }
+
+    const handleSubmit = async () => {
+        const response = await fetch(`http://localhost:5001/users?email=${formData.email}&password=${formData.password}`, { method: "GET" });
+        if (response.ok) {
+            const users = await response.json();
+            if (users.length > 0) {
+                alert("User found");
+            } else {
+                alert("Email/password is incorrect");
+            }
+        } else {
+            alert("Something went wrong, please try again");
+        }
+    }
+
     return (
         <div className='py-2'>
             <div className='mb-3'>
@@ -19,7 +34,7 @@ const Login = () => {
                 <label className='form-label'>Password</label>
                 <input type='password' className='form-control' name='password' onChange={handleChange} />
             </div>
-            <button className='btn btn-primary'>Login</button>
+            <button onClick={handleSubmit} className='btn btn-primary'>Login</button>
         </div>
     )
 }
